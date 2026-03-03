cbe03368feefc4d8051718cd734f2c094a9f8333
Rubish1229 <karmacharyarubish5@gmail.com>
2026-03-03 09:27:44 +0545

    Main method

---
+++ Main.java
@@ -0,0 +1,10 @@
+package LayoutExample;
+
+public class Main {
+    public static void main(String[] args) {
+//        new GridLayoutExample();
+//          new BorderLayoutExample();
+//        new FlowLayoutExample();
+        new GridLayoutExample();
+    }
+}
